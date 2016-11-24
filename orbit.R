f0a877c4086fc89a1a3bbc1ac41bd3b20b1d24a3
Joedang <shields6@pdx.edu>
2016-11-24 15:21:59 -0800

    incorporated 3-body plotting stuff
    Haven't run it yet, bug fixes incoming...

--- orbit.R
+++ orbit.R
@@ -9,24 +9,66 @@
 # load packages
 require("deSolve")
 require("scatterplot3d")
-
-G <- 1
 source("orbitFunctions.R")
-#### 3-Body Problem ####
-# bounce between L4 and L5:
-# yini <- c(cos(pi/3), sin(pi/3), 0, 0.01*runif(1), 0.01*runif(1), 0.01*runif(1))
-# parms <- list(r1=c(0, 0, 0), r2=c(1, 0, 0), m1=10, m2=0.1, omega= c(0,0,1)*sqrt(G*10/1))
-# tiny L3 orbit:
-# yini <- c(
-# 	  -1+7*0.1/12/10,	0.1,		0, 
-# 	  0,	0,	0.1
-# 	  )
-# parms <- list(r1=c(0, 0, 0), r2=c(1, 0, 0), m1=10, m2=0.1, omega= c(0,0,1)*sqrt(G*10/1))
-# yini <- c(0.6, 0, 0, 0.01*runif(1), 2+0.01*runif(1), 0.01*runif(1))
-parms <- list(r1=c(0, 0, 0), r2=c(1, 0, 0), m1=10, m2=0.1, omega= c(0,0,1)*sqrt(G*10/1))
-traj <- ode(y= yini, func=body3, times= seq(from=0, by=2e-3, to=3.87), parms=parms, method="rk4")
+
+#### Input parameters ####
+# strength of gravity:
+G <- 1
+# mass of the "sun":
+M1 <- 4
+# mass of the "planet":
+M2 <- 1
+# reduced mass for M1 and M2:
+mu <- M1*M2/(M1+M2)
+# distance from the CoM to M1:
+R1 <- -0.5
+# distance from the CoM to M2:
+R2 <- -M1*R1/M2
+# rotation rate of the reference frame:
+omega.z <- sqrt(G*(M1+M2)/abs(R2-R1)^3)
+# how many colors to use to plot the potential:
+n.colors <- 2^6
+# how much of the violet end of the colors to cut off:
+end.colors <- 0.8
+# how much of the red end of the colors to cut off:
+start.colors <- 0.05
+# value below which the potential is not drawn:
+pot.floor <- -7
+# color palette for the potential:
+pal <- rev(rainbow(n.colors, start= start.colors,end=end.colors)) 
+# time steps for the simulation:
+times <- seq(from=0, by=2e-3, to=3.87) 
+
+#### Definte the potential ####
+pot.grav <- function(x,y, x0, y0, M) -M*G/sqrt((x0-x)^2+(y0-y)^2)
+pot.CF <- function(x, y, omega.z) -1/2*omega.z^2*((x)^2+(y)^2)
+potential <- function(x,y) pot.CF(x, y, omega.z)+pot.grav(x, y, R1, 0, M1)+pot.grav(x, y, R2, 0, M2)
+potential.cap <- function(x,y)
+{
+	pot <- potential(x,y)
+	pot <- pot*(pot >= pot.floor ) +pot.floor*(pot < pot.floor)
+}
+
+#### Calculate potential field and make a color key ####
+x <- seq(from=-4, to=4, length.out=5e2)+0.75
+y <- seq(from=-3, to=3, length.out=5e2)
+pot.XY <- outer(x, y, potential.cap)
+cols <- seq(from= min(pot.XY, na.rm=T), to= max(pot.XY, na.rm=T), length.out= n.colors)
+cols.mat <- matrix(cols, nrow=1)
+
+#### Calculate the locations of the Lagrange points ####
+x.L4 <- (R2-R1)*cos(pi/3)+R1
+y.L4 <- (R2-R1)*sin(pi/3)
+x.L5 <- x.L4
+y.L5 <- -y.L4
+# use polyroot() to easily find the locations of L1 and L2
+
+#### Calculate the motion of the satellite ####
+parms <- list(r1=c(R1, 0, 0), r2=c(R2, 0, 0), m1=M1, m2=M2, omega= c(0,0,omega.z)*sqrt(G*10/1))
+traj <- ode(y= yini, func=body3, times= times, parms=parms, method="rk4")
 traj <- data.frame(traj)
 
+#### Check the satellite motion for kinks ####
 ang <- asin(angles(traj$X1, traj$X2, traj$X3))*360/2/pi
 cat(
     "Maximum step angle", max(ang), "degrees ocurring at step", which.max(ang), 
@@ -34,4 +76,4 @@ cat(
     )
 
 source("orbitPlot.R")
-# orbitPlot()
+system("cp orbit.pdf ~/Downloads/")
--- orbitPlot.R
+++ orbitPlot.R
@@ -5,19 +5,40 @@ if (!exists("GIF"))
 	GIF <- F
        	cat("Defaulting to no GIF creation...\n")
 }
-# orbitPlot <- function()
-# {
 pdf("orbit.pdf")
 par.def <- par(no.readonly=T)
 
-plot(traj$X1, traj$X2, asp=1, main= "trjectory plot", xlim=c(-1.5, 1.5), ylim=c(-1.5, 1.5), type="l", col=alpha("black", 0.5))
+#### Plot the potential field ###
+nf <- layout(matrix(c(1,2), nrow=1), c(5, 1) )
+image(
+      x, y, pot.XY, 
+      asp=1, col= pal, 
+      bty="n", axes= F, xlab=NA, ylab=NA
+      )
+# contour(x, y, pot.XY, col="lightgray", drawlabels=F, add=T)
+points(c(0,R1,R2,x.L4,x.L5), c(0,0,0,y.L4,y.L5), pch=c(8, 8, 8))
+text(x=c(0,R1,R2,x.L4,x.L5), y=c(0,0,0,y.L4,y.L5), labels=c("CoM", "M1", "M2", "L4", "L5"), pos=3, offset=0.3, cex=0.8, font=2)
+
+#### plot the trajectory of the satellite ####
+plot(traj$X1, traj$X2, asp=1, main= "trjectory plot", xlim=c(-1.5, 1.5), ylim=c(-1.5, 1.5), type="l", col=alpha("black", 0.5), add=T)
 points(traj$X1, traj$X2, col=alpha("red", 0.01), pch=".")
 # points(rep(parms$r1[1], 2), rep(parms$r1[2], 2), pch=c(1, 19))
 points(parms$r1[1],parms$r1[2], pch=1)
 points(parms$r1[1],parms$r1[2], pch=".")
 points(parms$r2[1],parms$r2[2], pch=10)
 
-# time domain of XYZ coords in rotating frame
+#### plot the color key ####
+par.old <- par(mar=c(5,0,5,3))
+image(
+      1, cols, cols.mat, 
+      col=pal, bty="n", axes= F,
+      xlab= "U(r)", zlim= c(pot.floor, max(pot.XY, na.rm=T))
+      )
+axis(4, at=c(min(cols), mean(cols), max(cols)), labels= c("LO", "MID", "HI"))
+par(par.old)
+
+
+#### time domain of XYZ coords in rotating frame ####
 layout(matrix(1:3, ncol=1))
 par.old <- par(mar=c(0,4,4,2))
 plot(traj$time, traj$X1, type="l", col="red", ylab="x")
@@ -27,6 +48,7 @@ par(mar=c(5,4,0,2))
 plot(traj$time, traj$X3, type="l", col="blue", ylab="z", xlab="t")
 par(par.old)
 
+#### smoothed histogram of X, Y, and Z coordinates ####
 layout(matrix(1:3, ncol=1))
 par.old <- par(mar=c(0,4,4,2))
 plot(density(traj$X1))
@@ -34,6 +56,7 @@ plot(density(traj$X2))
 plot(density(traj$X3))
 par(par.old)
 
+#### time domain and smoothed histogram of the kinetic energy ####
 layout(matrix(1:2, ncol=1))
 par.old <- par(mar=c(0,4,4,2))
 v <- c(0, sqrt(diff(traj$X1)^2 +diff(traj$X2)^2 +diff(traj$X3)^2 ))/diff(traj$time)[1]
@@ -42,6 +65,7 @@ plot(traj$time, KE, main="Kinetic Energy", type="l", ylab="KE/m")
 plot(density(KE))
 par(par.old)
 
+#### Z as a function of X, also as a function of Y ####
 layout(matrix(1:2, ncol=1))
 par.old <- par(mar=c(0,4,4,2))
 plot(traj$X1, traj$X3, asp=1, type="l")
@@ -53,6 +77,7 @@ scatterplot3d(traj$X1, traj$X2, traj$X3, scale.y=2, type="l")
 
 dev.off()
 
+#### make a GIF of the motion ####
 if (GIF)
 {
 	pdf("/tmp/orb%03d.pdf", onefile=F)
@@ -72,7 +97,5 @@ if (GIF)
 	dev.off()
 	system("bash orbitGIF.sh")
 }
-# system("")
 
 # system("cp orbit.pdf ~/Downloads/")
-# }
